9c2df3dc3291325f3e53ec22558938de0d41971e
Junniorfernandes <VALMIRFERNANDESCONTABILIDADE@GMAIL.COM>
2025-04-21 20:24:36 -0300

    Update AccountingApp.py

--- AccountingApp.py
+++ AccountingApp.py
@@ -511,14 +511,13 @@ def exportar_lancamentos_para_pdf(lancamentos_list, usuario_nome="Usuário"):
     signatario_cargo = st.session_state.get('usuarios', [])[st.session_state.get('usuario_atual_index', 0)].get("SignatarioCargo", "")
 
     if signatario_nome or signatario_cargo:
-    pdf.set_font("Arial", '', 10)
+        pdf.set_font("Arial", '', 10)
 
         if signatario_nome:
             pdf.cell(0, 10, f"Assinado por: {signatario_nome}", 0, 1, 'L')
 
         if signatario_cargo:
             pdf.cell(0, 8, signatario_cargo, 0, 1, 'L')
-
 
     pdf_output = pdf.output(dest='S')
     return io.BytesIO(pdf_output.encode('latin1'))
