d92a7255e07c724d74cbe615f324c28ca5af0f03
Junniorfernandes <VALMIRFERNANDESCONTABILIDADE@GMAIL.COM>
2025-04-21 21:35:02 -0300

    Update AccountingApp.py

--- AccountingApp.py
+++ AccountingApp.py
@@ -515,7 +515,7 @@ def exportar_lancamentos_para_pdf(lancamentos_list, usuario_nome="Usuário"):
 
         if signatario_nome:
             pdf.cell(0, 10, f"Assinado por: {signatario_nome}", 0, 1, 'C')
-        pdf.line(50, 100, 175, 100)
+        pdf.line(30, 100, 175, 100)
         pdf.cell(0, 15, "", 0, 1)  # Adiciona 15mm de espaço vertical
         if signatario_cargo:
             pdf.cell(0, 8, signatario_cargo, 0, 1, 'C')
