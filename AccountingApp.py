56d4245fa386cfd9f011297de8b0fc2faac9e8b3
Junniorfernandes <VALMIRFERNANDESCONTABILIDADE@GMAIL.COM>
2025-04-24 13:21:16 -0300

    Update AccountingApp.py

--- AccountingApp.py
+++ AccountingApp.py
@@ -831,7 +831,9 @@ def gerar_demonstracao_resultados_pdf(lancamentos_list, usuario_nome="Usuário")
     if receitas_por_categoria:
     	donut_path = criar_grafico_donut(receitas_por_categoria)
     	pdf.image(donut_path, x=55, y=pdf.get_y(), w=100)
-    	pdf.ln(90) 
+    	pdf.ln(90)
+	    
+    pdf.add_page()	    
 
     # --- Gráfico de Barras ----	
     plt.figure(figsize=(3, 3.2), facecolor='none') # Ajuste aqui largura x altura
